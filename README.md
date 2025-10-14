9cf45e93b1f5d1ee79fc34c2260e7a3b8268a43a
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-10-14 18:23:10 -0300

    feat: estudando literais

---
+++ README.md
@@ -0,0 +1,13 @@
+#  Bolsa de Estudos - IT For Girls | Cisco Python Essentials 1
+
+Este repositório reúne meus estudos e práticas desenvolvidas durante a bolsa de estudos **IT For Girls**, oferecida em parceria com a **Cisco**.  
+
+O curso **Python Essentials 1** faz parte do programa de formação e tem como objetivo introduzir os fundamentos da linguagem Python, lógica de programação e conceitos essenciais para desenvolvimento e automação.
+
+## Objetivo
+Registrar minha evolução, compartilhar aprendizado e consolidar o conhecimento adquirido durante o programa.
+
+
+**Programa:** IT For Girls — Bolsa de Estudos Cisco  
+**Curso:** Python Essentials 1  
+**Autora:** Vitória Ferreira  
--- olaMundo.py
+++ olaMundo.py
@@ -9,3 +9,38 @@ print("Monty Python")
 #parâmetro sep controla o que vai entre múltiplos argumentos dentro do mesmo print
 print("Meu nome é", "Python", sep="-") 
 print("Monty Python")
+#string multiplicada
+print("String" * 2)
+
+#Sitacoes com strings usando caracter de escape
+print("Eu gosto \"Monty Python\"")
+print('Eu gosto "Monty Python"')
+print("\"Eu sou\"\n\"\"aprendiz\"\"\n\"\"\"Python\"\"\"")
+
+#uso de apostrofos
+print('I\'m Monty Python.')
+
+
+
+
+#Numeros inteiros
+#conversao para numero octal
+print(0o123)
+#gexadeciaml
+print(0x123)
+print(4)
+
+#numeros decimais float
+print(0.4)
+print(.4)#pode omitir o 0 nessas situações
+
+#potencia usa-se 10 implicito - BASE, valor na frente do (e) / expoente valor apos o (e) e deve ser numero inteiro
+print(3e8) #Três vezes dez à potência de oito.
+
+#comportamento de floats pequenos/ ou grandes
+print(0.0000000000000000000001)
+
+#valores booleanos true 1 - 0 false
+print(True > False)
+print(True < False)
+

aea99f538c02ad153bb5d1c80cce0b08f4048db4
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-10-14 17:06:21 -0300

    first commit

---
+++ olaMundo.py
@@ -0,0 +1,11 @@
+#print por padrão adiciona uma quebra de linha ao final
+print("Olá mundo!")
+#funcao print com varios argumentos
+print("A aranha pequenininha" , "subiu" , "a tromba d'água.")
+#argumentos com palavras-chaves
+#parâmetro end quando colocado na funcao print, NÃO coloca a quebra de linha e o próximo print começa logo depois 
+print("Meu nome é", end="*") 
+print("Monty Python")
+#parâmetro sep controla o que vai entre múltiplos argumentos dentro do mesmo print
+print("Meu nome é", "Python", sep="-") 
+print("Monty Python")

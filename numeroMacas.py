5d562014e10647ddedc4ff861a86922b00c34b35
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-10-16 16:54:34 -0300

    feat: criando variaveis, usando operadores

---
+++ numeroMacas.py
@@ -0,0 +1,18 @@
+#Era uma vez em Appleland, John tinha três maçãs, Maria cinco maçãs e Adam tinha seis maças. 
+#Todos ficaram muito felizes e viveram por muito tempo. Fim da história.
+
+#Sua tarefa:
+
+#crie as variáveis: john, mary e adam;
+#atribuir valores às variáveis. Os valores devem ser iguais aos números de fruto possuído por John, Mary e Adam, respectivamente;
+#tendo armazenado os números nas variáveis, imprimindo as variáveis em uma linha e separando cada uma delas com uma vírgula;
+#Agora, crie uma nova variável chamada total_apples igual à adição das três variáveis anteriores.
+#imprima o valor armazenado em total_apples no console;
+john = 3;
+mary = 5;
+adam = 6;
+
+print(john, mary, adam, sep=",");
+
+total_apple = john + mary + adam;
+print(total_apple);

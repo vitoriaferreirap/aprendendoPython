428eba4afc0da6d7c3c56ab9facac0f9cfd77739
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-10-25 11:23:21 -0300

    feat: + como concatenador, * como operador replicacao

--- funcEntrada.py
+++ funcEntrada.py
@@ -3,3 +3,18 @@ print(name);
 
 #Erro, funcao input retorna apenas String
 #operacao = name - 2;
+
+#trabalhando com conversao de tipos
+#float aceita int, int NAO aceita float
+num = float(input("Digite um numero: "));
+operacao = num - 2;
+print(num, "subtraido por 2 é ", operacao)
+
+# + como concatenador
+a = "ab";
+b = "cd";
+print(a + b);
+
+# * como  operador de replicacao
+replica = a * 3;
+print(replica);

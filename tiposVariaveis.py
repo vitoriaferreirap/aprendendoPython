afe0ac2f262256761a1570dcd8869800b3e88c6b
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-10-25 11:31:24 -0300

    feat: entendendo a declaracao de variaveis em python

---
+++ tiposVariaveis.py
@@ -0,0 +1,16 @@
+#E python NAO é necessario declarar o tipo de variavel ecplicitamente.
+idade = 25;
+print(idade);
+
+idade = 30;
+print(idade);
+print(type(idade)); #descobre tipo
+
+idade = 10.5;
+print(idade);
+print(type(idade)); #descobre tipo
+
+idade = "s";
+print(idade);
+print(type(idade)); #descobre tipo
+

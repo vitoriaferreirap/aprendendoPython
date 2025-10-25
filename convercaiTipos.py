704aaa31ee6a34d7297f91d9f870ed6188fa31a2
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-10-25 11:43:06 -0300

    trabalhando com convercao de tipos

---
+++ convercaiTipos.py
@@ -0,0 +1,13 @@
+#trabalhando com conversao de tipos
+#float aceita int, int NAO aceita float
+num = float(input("Digite um numero: "));
+operacao = num - 2;
+print(num, "subtraido por 2 é ", operacao)
+
+
+numero = 4;
+print(type(numero));
+#convercao string
+numero = str(numero);
+print(numero);
+print(type(numero));
--- funcEntrada.py
+++ funcEntrada.py
@@ -4,12 +4,6 @@ print(name);
 #Erro, funcao input retorna apenas String
 #operacao = name - 2;
 
-#trabalhando com conversao de tipos
-#float aceita int, int NAO aceita float
-num = float(input("Digite um numero: "));
-operacao = num - 2;
-print(num, "subtraido por 2 é ", operacao)
-
 # + como concatenador
 a = "ab";
 b = "cd";
@@ -18,3 +12,4 @@ print(a + b);
 # * como  operador de replicacao
 replica = a * 3;
 print(replica);
+

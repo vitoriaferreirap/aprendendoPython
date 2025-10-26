939312b0707b59ac70148e2bbd054e2631eb107c
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-10-26 13:33:27 -0300

    feat:desicoes

---
+++ desicoes.py
@@ -0,0 +1,25 @@
+#  Operador igualdade
+var = 0 # Atribuicao
+print(var == 0) # Comparacao
+var = 1 
+print(var == 0)
+
+# Operador desigualdade
+var = 0  
+print(var != 0)
+var = 1 
+print(var != 0)
+
+# Operadores de commparacao :
+a = 10
+b = 5
+a > b  # Maior que
+a >= 0.0  # Maior ou igual a
+a < 85  # Menor que
+a <= 85  # Menos que ou igual a
+
+#escreva um programa simples de duas linhas que aceita o parâmetro n 
+#como entrada, que é um inteiro, e imprime False se n for menor 100,
+# e True se n for maior ou igual a 100.
+n = int(input("Digite um numero: "))
+print(n >= 100)
